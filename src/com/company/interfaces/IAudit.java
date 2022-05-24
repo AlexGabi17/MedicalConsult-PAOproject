021a212c20c277304f87ecef62c9771b5deeca96
Alexandru Buzescu <alexbuzescu@yahoo.com>
2022-05-24 17:30:33 +0300

    Stergere fisiere vechi

--- src/com/company/interfaces/IAudit.java
+++
@@ -1,5 +0,0 @@
-package com.company.interfaces;
-
-public interface IAudit {
-    public void writeAudit(String command,String entityType, int numberOfInsertions);
-}
--- src/com/company/interfaces/IRoom.java
+++
@@ -1,7 +0,0 @@
-package com.company.interfaces;
-
-import com.company.entities.Room;
-
-public interface IRoom {
-    public int getNumber(Room obj);
-}
